2d6993c59890a6b6e11fa36d356d322695581420
rodrigoarias12 <rodrigogonzaloarias@gmail.com>
2022-10-15 20:00:41 -0300

    Config project

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
-
-export default config;
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ require("dotenv").config();
+ require("@nomiclabs/hardhat-ethers");
+ require("@nomiclabs/hardhat-etherscan");
+ 
+ module.exports = {
+   solidity: "0.8.15",
+   defaultNetwork: "mumbai",
+   networks: {
+     hardhat: {},
+     mumbai: {
+       //ankr's free public rpc
+       url: "https://rpc.ankr.com/polygon_mumbai",
+       accounts: [`0x${process.env.PRIVATE_KEY}`],
+     },
+   },
+   etherscan: {
+     apiKey: {
+       polygon: process.env.POLYGONSCAN_API_KEY || "",
+       polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
+     },
+   },
+ };
